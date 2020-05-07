2740c8a65db9afe4b4eda2b8e756205adf672d22
travislykes <fopokuamoako@gmail.com>
2020-05-07 12:13:23 +0200

    define layouts for dashboard

---
+++ resources/js/layouts/admin.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+  import sidebar from '~/components/dashboard/Sidebar'
+  import navbar from '~/components/dashboard/Nav'
+  export default {
+    name: 'AdminLayout',
+
+    components:{
+      sidebar,
+      navbar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
